d6f5ff2fd47a8478afd16df2f1112cca533f0da7
WhatProblem <18355350915@163.com>
2018-09-24 22:32:48 +0800

    基础架构

--- public/index.php
+++ public/index.php
@@ -1,30 +1,25 @@
 <?php
-if (PHP_SAPI == 'cli-server') {
-    // To help the built-in PHP dev server, check if the request was actually for
-    // something which should probably be served as a static file
-    $url  = parse_url($_SERVER['REQUEST_URI']);
-    $file = __DIR__ . $url['path'];
-    if (is_file($file)) {
-        return false;
-    }
-}
+// https://www.thatyou.cn/%E4%BD%BF%E7%94%A8slim-php-mysql%E6%9E%84%E5%BB%BArestful-api/
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
 
-require __DIR__ . '/../vendor/autoload.php';
+header("Content-Type: text/html;application/json;image/jpeg;charset=utf-8");
+header("Access-Control-Allow-Origin:*");
+header("Access-Control-Allow-Headers: *");
+header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+header("X-Powered-By", ' 3.2.1');
+header("Access-Control-Allow-Credentials", "true");
+header("Access-Control-Expose-Headers", "*");
 
+require '../vendor/autoload.php';
 session_start();
-
-// Instantiate the app
-$settings = require __DIR__ . '/../src/settings.php';
+$settings = require '../src/settings.php';
 $app = new \Slim\App($settings);
 
-// Set up dependencies
-require __DIR__ . '/../src/dependencies.php';
+// Route and Dependencies
+require '../src/homeRoutes.php';
+require '../src/routes.php';
+require '../src/dependencies.php';
 
-// Register middleware
-require __DIR__ . '/../src/middleware.php';
-
-// Register routes
-require __DIR__ . '/../src/routes.php';
-
-// Run app
 $app->run();
+
--- src/settings.php
+++ src/settings.php
@@ -1,19 +1,23 @@
 <?php
+
+
 return [
     'settings' => [
         'displayErrorDetails' => true, // set to false in production
         'addContentLengthHeader' => false, // Allow the web server to send the content-length header
 
-        // Renderer settings
-        'renderer' => [
-            'template_path' => __DIR__ . '/../templates/',
-        ],
-
-        // Monolog settings
-        'logger' => [
-            'name' => 'slim-app',
-            'path' => isset($_ENV['docker']) ? 'php://stdout' : __DIR__ . '/../logs/app.log',
-            'level' => \Monolog\Logger::DEBUG,
+    
+         // Database connection settings           
+        "db" => [
+            "host" => "localhost",
+            "dbname" => "test",
+            "user" => "root",
+            "pass" => "",
+            "options" => array(
+                PDO::MYSQL_ATTR_INIT_COMMAND => "set names utf8"
+            )
         ],
     ],
 ];
+
+
